1fccc8dd9bf2879313b3c6e65b0e17ce764a549e
MateHUN <matehunter123@gmail.com>
2024-07-10 03:04:49 +0200

    feat: Teleport, Troll menu
    ## Teleport
    - ➕Design
    ## Troll
    - ➕Design

--- src/components/Teleport.vue
+++ src/components/Teleport.vue
@@ -1,10 +1,44 @@
 <template>
-     <div>
-          <h1>Teleport Content</h1>
-          <p>Welcome to the Teleport!</p>
+     <div class="flex justify-center items-center min-h-screen">
+          <div v-if="props.selectedPlayer" class="border-[3px] border-slate-600 p-8">
+
+               <div class="relative">
+                    <h1 class="text-lg font-bold text-white absolute -top-12 right-[5rem]">TELEPORT MENU</h1>
+               </div>
+
+               <div class="grid grid-cols-2 gap-4">
+                    <button v-for="(button, index) in buttons" :key="index" @click="handleAction(button)"
+                         class="inline-flex items-center justify-center whitespace-nowrap rounded-md ring-offset-slate-700 transition-colors focus-visible:outline-none focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-slate-700 text-white hover:bg-slate-600 h-10 px-4 py-2 border-2 border-black text-lg font-bold italic">
+                         {{ button.toUpperCase() }}
+                    </button>
+               </div>
+          </div>
+
+          <div v-else>
+               <h1 class="text-lg font-bold text-white">No player selected</h1>
+          </div>
      </div>
 </template>
 
 <script setup>
+
+const props = defineProps({
+     selectedPlayer: Object
+})
+
+const handleAction = (action) => {
+     console.log(`action: ${action}`)
+}
+
+const buttons = [
+     "Public",
+     "Cloth",
+     "Impound",
+     "Ambulance",
+     "Waypoint",
+     "LSPD",
+     "Bring",
+     "Goto"
+]
 
 </script>
